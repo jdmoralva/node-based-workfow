bf54fd1bb4c30c42ab63da64a06131553f187e4e
Jorge David Morales <jdmoralva@gmail.com>
2026-07-14 22:06:03 -0500

    Frontend/ deleted

--- frontend/assets/js/pages/card-grid-page.js
+++
@@ -1,9 +0,0 @@
-import { initCardSelectionController } from '../controllers/card-selection-controller.js';
-
-export function bootstrapCardGridPage(options = {}) {
-  return {
-    cardSelectionController: initCardSelectionController({
-      root: options.root || document,
-    }),
-  };
-}
--- frontend/assets/js/pages/creditmodeler-service-page.js
+++
@@ -1,9 +0,0 @@
-import { initTreeToggleController } from '../controllers/tree-toggle-controller.js';
-
-export function bootstrapCreditmodelerServicePage(options = {}) {
-  return {
-    treeToggleController: initTreeToggleController({
-      root: options.root || document,
-    }),
-  };
-}
